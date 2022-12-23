443e769e5f2ccd2cdfb7bf902b510e1911a81224
Meik <meik_seedorf@web.de>
2022-12-23 16:01:20 +0100

    changed to cmd interface

--- main.py
+++ main.py
@@ -1,182 +1,48 @@
-from numpy import random 
-import time 
-import xlwings as xw
-import kivy   
-from kivy.app import App  
-from kivy.uix.label import Label  
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+#!/usr/bin/env python3
 
-### DO CHANGES HERE IF YOU WANT DIFFERENT VOCAB ###
-path = r'.\Hanyu.odt'
-xw.Book(path).set_mock_caller()
-wb = xw.Book.caller()
-ws1 = wb.sheets['Tabelle1']
+# import sys 
 
-# initalize the font
-font_size = int(50)
-font_path = r".\DroidSansFallback.ttf"
+from tkinter import *
 
-# define a Girdlayout for the App
-# here all widgets and all methods are placed
-class MyGrid(GridLayout):
-    
-    # chose the right vocab from the sheet depending which lecture was chosen 
-    # vocab is chosen randomly from the lecture
-    row_list = [1]          
-    def vocab(self):
-        self.row = int(random.choice(self.row_list))
-        self.german = str(ws1.range('b%d'%self.row).value)
-        self.hanyu = str(ws1.range('d%d'%self.row).value)
-        self.pinyin = str(ws1.range('c%d'%self.row).value)
+class App(Tk):
 
-    def __init__(self, **kwargs):
+    def __init__(self):
+        super().__init__()
 
-        # initialize first vocab
-        self.vocab() 
+        self.geometry('{}x{}'.format(600, 400))   
+        # self.resizable(False, False)
 
-        super(MyGrid, self).__init__(**kwargs)
-        #### main grid
-        # define column (and usually rows) for main grid
-        self.cols= 1
+        self.columnconfigure(0, weight=1)
+        # self.columnconfigure(1, weight=1)
+        # self.columnconfigure(2, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        ### first cell
-        # define columns inside the first cell
-        self.first= GridLayout()
-        self.first.cols = 2
-        self.first.rows = 1
+        # # row 1
+        self.quest= Label(self, text="question")
+        self.answer= Label(self, text="result")
 
-        # label to show vocab
-        self.label = Label(text = self.pinyin, font_name = font_path, font_size = font_size)
+        self.quest.grid(row=0, column=0, sticky="NSEW")
+        self.answer.grid(row=0, column=1, sticky="NSEW")
+        # row 2
 
-        # textinput to enter vocab  
-        self.textinput_vocab = TextInput(hint_text=u"german", font_name = font_path, font_size=font_size) 
-        self.textinput_vocab.bind(on_text_validate=self.on_enter)
-        self.textinput_vocab.multiline = False
-        self.textinput_vocab.text_validate_unfocus = False
-        self.textinput_vocab.write_tab = False
+        self.entry= Entry(self)
+        self.button = Button(self, text="enter")
 
-        # add all attributes in the first cell
-        self.first.add_widget(self.label)
-        self.first.add_widget(self.textinput_vocab)
-        self.add_widget(self.first)
+        self.entry.grid(row=1, column=1, sticky="NSEW")
+        self.button.grid(row=1, column=2, sticky="NSEW"))
+        # row 3
+        self.german = Radiobutton(self, text="german")
+        self.hanyu  = Radiobutton(self, text="hanyu")
+        self.pinyin = Radiobutton(self, text="pinyin")
 
-        ### second cell
-        # define columns inside the second cell
-        self.second= GridLayout()
-        self.second.cols = 1
-        self.second.rows = 2
+        self.german.grid(row=2, column=0, sticky="NSEW")
+        self.hanyu.grid(row=2, column=1, sticky="NSEW")
+        self.pinyin.grid(row=2, column=2, sticky="NSEW")
 
-        ## third cell
-        # define columns inside third cell
-        self.third = GridLayout()
-        self.third.cols = 2 
-        self.third.rows = 1
 
-        # button for switching languages
-        self.button_left = Button(text =u"chinese (pinyin)", font_name = font_path, font_size = font_size*0.5) 
-        self.button_left.bind(on_press=self.pressed_left)
+def main():
+    trainer = App()
+    trainer.mainloop()
 
-        # button for switching between hanyu and pinyin
-        self.button_right = Button(text =u"german", font_name = font_path, font_size = font_size*0.5)
-        self.button_right.bind(on_press=self.pressed_right)
-
-        # add all attributes to the thrid cell
-        self.third.add_widget(self.button_left)
-        self.third.add_widget(self.button_right)
-        # add the third to the second cell
-        self.second.add_widget(self.third)
-
-        # textinput to choose lecture
-        self.textinput_lecture = TextInput(hint_text=u"choose lecture", font_size=font_size*0.5)
-        self.textinput_lecture.bind(on_text_validate=self.choose_lecture)
-        self.textinput_lecture.multiline = False
-        self.textinput_lecture.write_tab = False
-
-        # add all attributes to the second cell
-        self.second.add_widget(self.textinput_lecture)
-        # add second cell to the main grid
-        self.add_widget(self.second)
-
-    # set range which vocab shall be learned 
-    def choose_lecture(self, instance):
-        listvalue = int(self.textinput_lecture.text)
-        self.row_list = []
-        for row in range(1,2000):
-            value = ws1.range('a%d'%row).value 
-            if value == listvalue:
-                self.row_list.append(row)
-
-    # initialize flags for the languages
-    flag_left= 2    # chose the right flag and than add 1 
-    flag_right = 1
-    flag_enter = 1
-
-    # chose label language
-    def pressed_left(self, instance):
-        if self.flag_left== 0:
-            self.button_left.text = "german"
-            self.label.text = self.german
-            self.flag_left = 1 
-        elif self.flag_left== 1:
-            self.button_left.text = "chinese (pinyin)"
-            self.label.text = self.pinyin
-            self.flag_left = 2
-        elif self.flag_left == 2:
-            self.button_left.text = "chinese (汉语)"
-            self.label.text = self.hanyu
-            self.flag_left = 0
-
-    # chose language for the textinput
-    def pressed_right(self, instance):
-        if self.flag_right == 0:
-            self.button_right.text = "german"
-            self.textinput_vocab.hint_text = "german"
-            self.flag_right = 1 
-        elif self.flag_right == 1:
-            self.button_right.text = "chinese (pinyin)"
-            self.textinput_vocab.hint_text = "chinese (pinyin)"
-            self.flag_right = 2
-        elif self.flag_right == 2:
-            self.button_right.text = "chinese (汉语)"
-            self.textinput_vocab.hint_text = "chinese (汉语)"
-            self.flag_right = 0
-
-    # execute the comparison between the languages 
-    def on_enter(self, instance):
-        if self.flag_enter == 1:
-            if self.flag_right == 1:
-                answer = self.german
-            elif self.flag_right == 2:
-                answer = self.pinyin
-            elif self.flag_right == 0:
-                answer = self.hanyu
-            # examine if vocab was chosen correctly
-            if self.textinput_vocab.text == answer:
-                self.label.color = [0, 1, 0, 1]
-            else:
-                self.label.color = [1, 0, 0, 1]
-                self.label.text = self.label.text + "\n" + "[" + answer + "]"                
-            self.flag_enter = 0
-        else:
-            self.vocab()
-            if self.flag_left == 1:
-                new_vocab = self.german
-            elif self.flag_left == 2:
-                new_vocab = self.pinyin
-            elif self.flag_left == 0:
-                new_vocab = self.hanyu
-            self.label.text = new_vocab
-            self.textinput_vocab.text = ""
-            self.label.color = [1, 1, 1, 1]
-            self.flag_enter= 1
-
-# build the App
-class MyApp(App): 
-    def build(self):
-        return MyGrid()
- 
-# run the App 
-if __name__ == "__main__": 
-    MyApp().run() 
+if __name__ == "__main__":
+    main()
