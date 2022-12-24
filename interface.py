17f3928cdd3c4cab6286060da8734e571c465119
Meik <meik_seedorf@web.de>
2022-12-24 13:18:44 +0100

    only lang change missing

--- interface.py
+++ interface.py
@@ -3,46 +3,75 @@
 # import sys 
 
 from tkinter import *
+from engine import Engine
 
-class App(Tk):
+class App(Tk, Engine):
 
     def __init__(self):
-        super().__init__()
+
+        Tk.__init__(self)
+        Engine.__init__(self)
 
         # self.geometry('{}x{}'.format(600, 400))   
         # self.resizable(False, False)
 
-        # self.columnconfigure(0, weight=1)
-        # self.columnconfigure(1, weight=1)
-        # self.columnconfigure(2, weight=1)
-        # self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=5)
+
+        # config
         font = "Calibri {}".format(30)
+        lang= ['german', 'pinyin', 'hanyu']
+        string = " ".join(map(str, lang))
+        var = StringVar(value=string)
 
         # # row 1
         self.quest= Label(self, text="target", font = font)
         self.answer= Label(self, text="result", font = font)
 
         self.quest.grid(row=0, column=0)
-        self.answer.grid(row=0, column=2)
+        self.answer.grid(row=0, column=1)
         # row 2
-
         self.entry= Entry(self, font = font)
+        self.entry.bind('<Key-Return>', self.check_result)
         self.button = Button(self, text="enter", font = font)
-        self.button.bind('<Key-Return>', self.check_result)
+        self.button.bind('<Button>', self.check_result)
 
-        self.entry.grid(row=1, column=1)
-        self.button.grid(row=1, column=2)
+        self.entry.grid(row=1, column=0)
+        self.button.grid(row=1, column=1)
         # row 3
-        self.german = Radiobutton(self, text="german")
-        self.hanyu  = Radiobutton(self, text="hanyu")
-        self.pinyin = Radiobutton(self, text="pinyin")
+        self.target=Listbox(self, listvariable = var)
+        self.target.grid(row=2, column=0)
+        self.input=Listbox(self, listvariable = var)
+        self.input.grid(row=2, column=1)
 
-        self.german.grid(row=2, column=0)
-        self.hanyu.grid(row=2, column=1)
-        self.pinyin.grid(row=2, column=2)
+        self.flag = True
+        # inital load
+        self.update_quest()
+
+    def update_quest(self):
+        self.load()         # get self.index
+        self.quest.config(text=self.lecture[self.index][self.flag_q])
 
     def check_result(self, event):
-        self.answer.config(fg="green")
+        self.check_lanuage()
+        if self.flag:
+            if self.entry.get() == "":
+                self.answer.config(text="")
+            else:
+                string = self.eval(self.entry.get())
+                if string == "correct":
+                    self.answer.config(text=string, fg = "green")
+                else:
+                    self.answer.config(text=string, fg = "red")
+            self.flag = False
+        else:
+            self.answer.config(text="", fg = "black")
+            self.entry.delete(0, END)
+            self.update_quest()
+            self.flag = True
+
+    def check_lanuage(self):
+        self.answer.config(text=self.target.get(0))
 
 
 def main():
